570b701f1602f04bba48dea752d935180c6a6025
Diego Castro Rendón <go@Diegos-MacBook-Air.local>
2024-10-02 19:59:01 -0600

    cambios pero hay conflictos

--- src/app/modules/product/component/category/category.component.ts
+++ src/app/modules/product/component/category/category.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Asegúrate de importar CommonModule
 import { CategoryService } from '../../_service/category.service';
 import { Category } from '../../_model/category';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-category',
@@ -10,18 +12,46 @@ import { Category } from '../../_model/category';
   standalone: true,  // Componente autónomo
   imports: [CommonModule]  // Importa CommonModule para poder usar *ngFor
 })
-export class CategoryComponent implements OnInit {
-  categories: Category[] = [];
+//export class CategoryComponent implements OnInit {
+// categories: Category[] = [];
 
-  constructor(private categoryService: CategoryService) {}
+//  constructor(private categoryService: CategoryService) {}
 
   // Método que llama al servicio para obtener las categorías
-  getCategories(): void {
-    this.categories = this.categoryService.getCategories();
-  }
+//  getCategories(): void {
+//    this.categories = this.categoryService.getCategories();
+//  }
 
   // Ejecuta el método getCategories cuando el componente se inicializa
-  ngOnInit(): void {
-    this.getCategories();
+//  ngOnInit(): void {
+//    this.getCategories();
+//  }
+//}
+
+declare var $: any;
+
+export class CategoryComponent implements OnInit {
+  categoryForm: FormGroup;
+
+  constructor() {
+    this.categoryForm = new FormGroup({
+      category: new FormControl('', [Validators.required]),
+      tag: new FormControl('', [Validators.required])
+    });
+  }
+
+  // Método para abrir el modal 
+  openModal() {
+    $('#categoryModal').modal('show');
+  }
+
+  onSubmit() {
+    if (this.categoryForm.valid) {
+      // Mostramos mensaje de éxito con SweetAlert
+      Swal.fire('Éxito', 'Categoría añadida correctamente', 'success');
+    } else {
+      // Mostramos errores en los campos
+      Swal.fire('Error', 'Por favor completa todos los campos', 'error');
+    }
   }
 }
